015356483a169d6c873d8b34711ebfbba766f549
alu0100898293 <alu0100898293@ull.edu.es>
2022-01-07 13:48:20 +0000

    Añadidos botones con eventos, y versión incial de accesibilidad

--- PracticaGatsby/gatsby-config.js
+++ PracticaGatsby/gatsby-config.js
@@ -1,3 +1,16 @@
+const path = require("path");
+// Get paths of Gatsby's required rules, which as of writing is located at:
+// https://github.com/gatsbyjs/gatsby/tree/fbfe3f63dec23d279a27b54b4057dd611dce74bb/packages/
+// gatsby/src/utils/eslint-rules
+const gatsbyRequiredRules = path.join(
+  process.cwd(),
+  "node_modules",
+  "gatsby",
+  "dist",
+  "utils",
+  "eslint-rules"
+);
+
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
@@ -11,7 +24,20 @@ module.exports = {
       options : {
         name: `data`,
         path: `${__dirname}/src/data/`,
-        ignore: [`**/\.*`], //ignorar ficheros que comienzen por un punto
+        ignore: [`**/.*`], //ignorar ficheros que comienzen por un punto
+      },
+    },
+    {
+      resolve: "gatsby-plugin-eslint",
+      options: {
+        // Gatsby required rules directory
+        rulePaths: [gatsbyRequiredRules],
+        // Default settings that may be ommitted or customized
+        stages: ["develop"],
+        extensions: ["js", "jsx", "ts", "tsx"],
+        exclude: ["node_modules", "bower_components", ".cache", "public"],
+        // Any additional eslint-webpack-plugin options below
+        // ...
       },
     },
   ],
--- PracticaGatsby/src/components/eventos.js
+++ PracticaGatsby/src/components/eventos.js
@@ -11,19 +11,45 @@ const badgeStyle = {
     borderRadius: 4,
     padding: "4px 6px",
     display: "inline-block",
-    position: "relative",
     top: -2,
-    marginLeft: 10,
+    marginTop: 5,
+    marginBottom: 5,
     lineHeight: 1,
 }
 const panelStyle = {
-    backgroundColor: "#088413",
+    textAlign: "left",
+    display: "inline-block",
+}
+const eventoStyle = {
+    width: 245,
+    marginLeft: 4,
+    backgroundColor: "#76d7c4",
     border: "1px solid #088413",
     borderRadius: 4,
     padding: "4px 6px",
+    float: "left",
+    textAlign: "center",
 }
-const eventoStyle = {
-
+const imgStyle = {
+    border: "2px solid #1f618d",
+    marginTop: 3,
+    marginBottom: 3,
+    maxWidth: 160,
+    maxHeight: 160,
+}
+const buttonStyle = {
+    color: "#fff",
+    backgroundColor: "#117a65",
+    display: "block",
+    fontSize: 14,
+    fontWeight: "bold",
+    letterSpacing: 1,
+    borderRadius: 6,
+    marginLeft: "auto",
+    marginRight: "auto",
+}
+const displayNoneStyle = {
+    display: "none",
 }
 
 const Eventos = () => {
@@ -45,29 +71,44 @@ const Eventos = () => {
         }
     `)
 
+    let refs = [];
+
+    for(var i=0; i<4; i++)
+        refs.push(React.createRef());
+
     console.log({data});
+    let visible= [false,false,false,false];
+
+    const showMore = (i) =>{
+        visible[i] = !visible[i];
+        if(visible[i])
+            refs[i].current.style = "display: block";
+        else
+            refs[i].current.style = "display: none";
+    };
 
     return (
         <div style={panelStyle}>
             {data.allInfoEventosJson.edges.map( (edge,i) => (
                 <div key={edge.node.id} style={eventoStyle}>
-                    <p>
-                        <span style={badgeStyle}>Evento:</span>
-                        <span>{edge.node.nombre}</span>
-                    </p>
-                    <p>
-                        <span style={badgeStyle}>Fecha:</span>
-                        <span>{edge.node.fecha}</span>
-                    </p>
-                    <img src={edge.node.imagen} alt={edge.node.altImagen} />
-                    <p>
-                        <span style={badgeStyle}>Descripcion:</span>
-                        <span>{edge.node.descripcion}</span>
-                    </p>
-                    <p>
-                        <span style={badgeStyle}>Duracion:</span>
-                        <span>{edge.node.duracion}</span>
-                    </p>
+
+                    <p style={badgeStyle}>Evento:</p>
+                    <p>{edge.node.nombre}</p>
+
+                    <p style={badgeStyle}>Fecha:</p>
+                    <p>{edge.node.fecha}</p>
+                    
+                    <img src={edge.node.imagen} alt={edge.node.altImagen} style={imgStyle} /><br/>
+
+                    <button style={buttonStyle} onClick={() =>showMore(i)} >Mostrar detalles</button>
+
+                    <div style = {displayNoneStyle} ref={refs[i]}>
+                        <p style={badgeStyle}>Descripcion:</p>
+                        <p>{edge.node.descripcion}</p>
+                        
+                        <p style={badgeStyle}>Duracion:</p>
+                        <p>{edge.node.duracion}</p>
+                    </div>
                 </div>
             ))}
         </div>
